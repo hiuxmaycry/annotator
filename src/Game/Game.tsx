8af96606c2823181bcec219d61229889d650e962
Nicolas Walter Araya <nwaraya@gmail.com>
2022-05-22 19:45:53 -0300

    Add separator (#1)

--- src/Game/Game.tsx
+++ src/Game/Game.tsx
@@ -3,10 +3,18 @@ import { useState } from 'react';
 const Game = () => {
   const [ homeScore, setHomeScore ] = useState(0);
   const [ visitScore, setVisitScore ] = useState(0);
-  const onHomeIncreasePoint = () => { setHomeScore(homeScore + 1) };
-  const onVisitIncreasePoint = () => { setVisitScore(visitScore + 1) };
-  const onHomeDecreasePoint = () => { setHomeScore(homeScore? homeScore - 1: 0) };
-  const onVisitDecreasePoint = () => { setVisitScore(visitScore? visitScore - 1: 0) };
+  const onHomeIncreasePoint = () => {
+    setHomeScore(homeScore + 1)
+  };
+  const onVisitIncreasePoint = () => {
+    setVisitScore(visitScore + 1)
+  };
+  const onHomeDecreasePoint = () => {
+    setHomeScore(homeScore? homeScore - 1: 0)
+  };
+  const onVisitDecreasePoint = () => {
+    setVisitScore(visitScore? visitScore - 1: 0)
+  };
   const onResetPoints = () => {
     setHomeScore(0);
     setVisitScore(0);
@@ -21,8 +29,7 @@ const Game = () => {
       </div>
       <div className="middle-panel">
         <button onClick={onResetPoints}>reset</button>
-        <span>{homeScore}</span>
-        <span>{visitScore}</span>
+        <span>{homeScore}-{visitScore}</span>
       </div>
       <div className="right-panel">
         <span>Visitors</span>
