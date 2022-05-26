a713a82cf36e7e5fc427ef0add22f7be567f4a2c
Nicolas Walter Araya <nwaraya@gmail.com>
2022-05-26 10:43:47 -0300

    Add eslint to the project (#5)

--- src/Game/Game.tsx
+++ src/Game/Game.tsx
@@ -1,19 +1,19 @@
 import { useState } from 'react';
 
 const Game = () => {
-  const [ homeScore, setHomeScore ] = useState(0);
-  const [ visitScore, setVisitScore ] = useState(0);
+  const [homeScore, setHomeScore] = useState(0);
+  const [visitScore, setVisitScore] = useState(0);
   const onHomeIncreasePoint = () => {
-    setHomeScore(homeScore + 1)
+    setHomeScore(homeScore + 1);
   };
   const onVisitIncreasePoint = () => {
-    setVisitScore(visitScore + 1)
+    setVisitScore(visitScore + 1);
   };
   const onHomeDecreasePoint = () => {
-    setHomeScore(homeScore? homeScore - 1: 0)
+    setHomeScore(homeScore ? homeScore - 1 : 0);
   };
   const onVisitDecreasePoint = () => {
-    setVisitScore(visitScore? visitScore - 1: 0)
+    setVisitScore(visitScore ? visitScore - 1 : 0);
   };
   const onResetPoints = () => {
     setHomeScore(0);
@@ -24,20 +24,24 @@ const Game = () => {
     <div className="app-container">
       <div className="left-panel">
         <span>Home</span>
-        <button onClick={onHomeIncreasePoint}>+</button>
-        <button onClick={onHomeDecreasePoint}>-</button>
+        <button type="button" onClick={onHomeIncreasePoint}>+</button>
+        <button type="button" onClick={onHomeDecreasePoint}>-</button>
       </div>
       <div className="middle-panel">
-        <button onClick={onResetPoints}>reset</button>
-        <span>{homeScore}-{visitScore}</span>
+        <button type="button" onClick={onResetPoints}>reset</button>
+        <span>
+          {homeScore}
+          -
+          {visitScore}
+        </span>
       </div>
       <div className="right-panel">
         <span>Visitors</span>
-        <button onClick={onVisitIncreasePoint}>+</button>
-        <button onClick={onVisitDecreasePoint}>-</button>
+        <button type="button" onClick={onVisitIncreasePoint}>+</button>
+        <button type="button" onClick={onVisitDecreasePoint}>-</button>
       </div>
     </div>
   );
-}
+};
 
 export default Game;
