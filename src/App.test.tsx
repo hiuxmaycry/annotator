6cb5fd6aeb4cc0172eb6356c3cafd49f6a17e5cd
Nicolas Walter Araya <nwaraya@gmail.com>
2022-06-11 20:26:13 -0300

    Add unit test with testing library

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders the document title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Annotator/i);
 
   expect(linkElement).toBeInTheDocument();
 });
